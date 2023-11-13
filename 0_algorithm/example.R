a3dfa89e2be8d87a18443099ffc058f264b142c6
yuyi13 <raymond.yy.nash13@gmail.com>
2023-11-13 12:17:26 +1100

    update example

--- 0_algorithm/example.R
+++ 0_algorithm/example.R
@@ -8,14 +8,11 @@ for (k in requiredPackages){
 }
 
 # load ubESTARFM
-source('0_code/ubESTARFM.R')
+source('0_algorithm/ubESTARFM.R')
 
 # set a cluster to allow ubETSRAFM to run in parallel
 cl = makeCluster(4)
 registerDoParallel(cl)
-
-# set if you want to run in baseline mode or unbiased mode
-method = 'zero bias' # or you can set it as 'baseline'
 
 # set the paths of input, tmp and output
 data_path = '1_test_data/'
@@ -31,9 +28,11 @@ rst_coarse0 = raster(paste0(data_path, 'MOD11A1_LST_cloudrm_20160218.tif')) # co
 
 # run the ubESTARFM, and it will write into the out_path
 # some parameters are set as default values, you can change them as you see fit
+# set if you want to run in baseline mode or unbiased mode
+# method = 'zero bias'; or you can set it as 'baseline', which means ESTARFM
 
 ubESTARFM(w = 25, DN_min = 250, DN_max = 350, patch_long = 200, tmp_path, out_path, method='zero bias',
           rst_fine1, rst_fine2, rst_coarse1, rst_coarse2, rst_coarse0)
 
-# close the cluster
+# stop the cluster
 stopCluster(cl)
